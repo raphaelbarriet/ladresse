a8d21eaba0b75f43fc05ab1dfac32d109cd1bfc4
raphael <raphael.barriet@icloud.com>
2020-06-09 10:26:18 +0200

    coverImage test

---
+++ src/Controller/App/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\App;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController {
+
+    /**
+     * @Route("/", name="home.index")
+     */
+    public function index()
+    {
+        return $this->render("navigation/index.html.twig");
+    }
+
+}
